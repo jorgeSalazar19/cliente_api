e88efd873c96b69adbb562db4d32b0b806ec2a0f
jorgesalazar19 <jorgemsm12316@utp.edu.co>
2018-03-22 12:54:14 -0500

    changes

--- cliente_api/settings.py
+++ cliente_api/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = '/static'
+STATIC_ROOT = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
